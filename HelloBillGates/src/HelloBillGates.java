85c226cf4cef03ed5a732e55b9af1addbdf1581e
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2019-10-28 23:20:17 +0700

    Hello BillGates

---
+++ HelloBillGates/src/HelloBillGates.java
@@ -0,0 +1,11 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Scanner;
+public class HelloBillGates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        name =  scanner.nextLine();
+        System.out.println("Your name is "+name);
+    }
+}
