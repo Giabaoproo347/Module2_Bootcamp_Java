517f6637da3bb8407fb2acab8744b5745b8c5c08
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2019-10-30 00:09:10 +0700

    FindValueRandom IN ARRRAY

---
+++ FindMaxValueInArray/src/FindMaxValueInArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FindMaxValueInArray {
+    public static void main(String[] args) {
+        FindLagest findlagest = new FindLagest;
+    }
+}
+class FindLagest{
+    Scanner scanner = new Scanner(System.in);
+    int size;
+    int[] array;
+
+    public void findMaxValue() {
+
+    }
+}
