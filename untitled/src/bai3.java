4de05d4b797fa98b0bc716c80e7d37c113bd282c
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2019-10-30 10:48:56 +0700

    ABC

---
+++ untitled/src/bai3.java
@@ -0,0 +1,11 @@
+public class bai3 {
+    private String specificName = "Cat";
+    public String name;
+    private int leg = 4;
+    Cat() {
+
+    }
+    public  void setName (String name ) throws
+}
+
+
