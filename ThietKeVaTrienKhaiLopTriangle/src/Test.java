e1d353e9775d9d9d5dce155676d12e5f0f88d9a8
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2019-10-31 22:20:05 +0700

    Chu vi, dien tich tam giac

---
+++ ThietKeVaTrienKhaiLopTriangle/src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle("blue",false,3,4,5);
+        System.out.println(triangle.getArea());
+    }
+}
