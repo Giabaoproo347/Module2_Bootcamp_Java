de93d49ae07c14fd8aff3f8a7e8dabfeddaad03f
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2019-11-02 20:26:46 +0700

    Kiem tra nam nhuan

--- NumberToString/src/NumberToString.java
+++ NumberToString/src/NumberToString.java
@@ -1,10 +1,45 @@
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class NumberToString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao so thu 1");
         int num = scanner.nextInt();
-        String string;
-        
+        System.out.println("Nhap vao so thu");
+        String number = scanner.nextLine();
+
+        if (num > 0) {
+            if (num < 10) {
+                switch (num) {
+                    case 1:
+                        System.out.println("one");
+                        break;
+                    case 2:
+                        System.out.println("two");
+                        break;
+                    case 3:
+                        System.out.println("thwree");
+                        break;
+                    case 4:
+                        System.out.println("four");
+                        break;
+                    case 5:
+                        System.out.println("five");
+                        break;
+                    case 6:
+                        System.out.println("six");
+                        break;
+                    case 7:
+                        System.out.println("seven");
+                        break;
+                    case 8:
+                        System.out.println("eight");
+                        break;
+                    case 9:
+                        System.out.println("nine");
+                        break;
+
+        }
 
     }
-}
